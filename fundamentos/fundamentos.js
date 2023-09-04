f409a80755c6e9407797b8233d400824b69369c3
Carlos Eduardo <carlosouzavalle@gmail.com>
2023-09-03 22:52:28 -0300

    second commit

---
+++ fundamentos/fundamentos.js
@@ -0,0 +1,31 @@
+// Fundmaentos
+// var nome = "valor"
+var minhaVariavel = "Hello world";
+console.log(minhaVariavel);
+
+//Variaveis e tipos de dados
+var meuNUmero = 10; //numerico
+var meuNUmero2 = "10"; //texto
+
+console.log(meuNUmero + 2);  // soma
+console.log(meuNUmero2 + 3); // concatenação
+console.log(typeof meuNUmero2);
+// typeof indentifica o tipo de dado
+
+
+//bolean dois valores true e false 
+var cafe = true; // false
+console.log(cafe);
+console.log(typeof cafe);
+
+//object, arrray, null
+var meuObjeto = {};
+var meuArray = [];
+var meuNull = null; //para iniciar uma variavel sem valor
+var meuUndefined = undefined; //se uma variavel não tem valor ela começa undefined
+
+console.log(typeof meuObjeto);
+console.log(typeof meuArray);
+console.log(typeof meuNull);
+
+//O javascript interpreta varios dados como object 
