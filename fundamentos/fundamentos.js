ce1e4b0cb1b1f79fdaa96489893488dd3754c9f9
Carlos Eduardo <carlosouzavalle@gmail.com>
2023-09-08 16:20:09 -0300

    five day

--- fundamentos/fundamentos.js
+++ fundamentos/fundamentos.js
@@ -1,98 +1,191 @@
-// Fundmaentos
-// var nome = "valor"
-var minhaVariavel = "Hello world";
-console.log(minhaVariavel);
+// // Fundmaentos
+// // var nome = "valor"
+// var minhaVariavel = "Hello world";
+// console.log(minhaVariavel);
 
-//Variaveis e tipos de dados
-var meuNUmero = 10; //numerico
-var meuNUmero2 = "10"; //texto
+// //Variaveis e tipos de dados
+// var meuNUmero = 10; //numerico
+// var meuNUmero2 = "10"; //texto
 
-console.log(meuNUmero + 2);  // soma
-console.log(meuNUmero2 + 3); // concatenação
-console.log(typeof meuNUmero2);
-// typeof indentifica o tipo de dado
-
-
-//bolean dois valores true e false 
-var cafe = true; // false
-console.log(cafe);
-console.log(typeof cafe);
-
-//object, arrray, null
-var meuObjeto = {};
-var meuArray = [];
-var meuNull = null; //para iniciar uma variavel sem valor
-var meuUndefined = undefined; //se uma variavel não tem valor ela começa undefined
-
-console.log(typeof meuObjeto);
-console.log(typeof meuArray);
-console.log(typeof meuNull);
-
-//O javascript interpreta varios dados como object 
-
-const meuNumero = "123";
-
-const meuNovoNumero = Number(meuNumero);
-
-//O number pega o content da variavel e se for possivel ele converte
-
-console.log(typeof meuNumero);
-console.log(typeof meuNovoNumero);
-
-// estruturas de condição if, else, else if
+// console.log(meuNUmero + 2);  // soma
+// console.log(meuNUmero2 + 3); // concatenação
+// console.log(typeof meuNUmero2);
+// // typeof indentifica o tipo de dado
 
 
-const idadeUsuario = 16;
+// //bolean dois valores true e false 
+// var cafe = true; // false
+// console.log(cafe);
+// console.log(typeof cafe);
 
-if(idadeUsuario < 14) 
-{
-    console.log("criança");
-} 
-else if(idadeUsuario < 18) {
-    console.log("adolecente");
-}
-else {
-    console.log("Pode passar");
-}
+// //object, arrray, null
+// var meuObjeto = {};
+// var meuArray = [];
+// var meuNull = null; //para iniciar uma variavel sem valor
+// var meuUndefined = undefined; //se uma variavel não tem valor ela começa undefined
 
-//SWITCH    
+// console.log(typeof meuObjeto);
+// console.log(typeof meuArray);
+// console.log(typeof meuNull);
 
-const fruta = "manga";
+// //O javascript interpreta varios dados como object 
 
-switch(fruta) {
-    case "banana":
-        console.log("banana é a fruta");
-        break;
-    case "manga":
-        console.log("manga é a fruta");
-        break;  
-    default: 
-        console.log("fruta não encontrada");
-}
+// const meuNumero = "123";
 
-//LAÇOS DE REPETIÇÃO
+// const meuNovoNumero = Number(meuNumero);
 
-//contador, condição de limite, incremento
-for(let i = 0; i < 5; i++) {
-    console.log("o valor de pedidos é " + i);
-} 
+// //O number pega o content da variavel e se for possivel ele converte
+
+// console.log(typeof meuNumero);
+// console.log(typeof meuNovoNumero);
+
+// // estruturas de condição if, else, else if
 
 
-//WHILE 
-let k = 0;
+// const idadeUsuario = 16;
 
-while (k < 5) {
-    console.log("foram vendidos " + k)
-    k++;
-}
+// if(idadeUsuario < 14) 
+// {
+//     console.log("criança");
+// } 
+// else if(idadeUsuario < 18) {
+//     console.log("adolecente");
+// }
+// else {
+//     console.log("Pode passar");
+// }
+
+// //SWITCH    
+
+// const fruta = "manga";
+
+// switch(fruta) {
+//     case "banana":
+//         console.log("banana é a fruta");
+//         break;
+//     case "manga":
+//         console.log("manga é a fruta");
+//         break;  
+//     default: 
+//         console.log("fruta não encontrada");
+// }
+
+// //LAÇOS DE REPETIÇÃO
+
+// //contador, condição de limite, incremento
+// for(let i = 0; i < 5; i++) {
+//     console.log("o valor de pedidos é " + i);
+// } 
 
 
-//do while
-let j = 0;
+// //WHILE 
+// let k = 0;
 
-do {
+// while (k < 5) {
+//     console.log("foram vendidos " + k)
+//     k++;
+// }
 
-    console.log("mais coisas foram vendidas " + j)
-    j++
 
-} while(j < 5);
+// //do while
+// let j = 0;
+
+// do {
+
+//     console.log("mais coisas foram vendidas " + j)
+//     j++
+
+// } while(j < 5);
+
+// //function
+// // function name(arg1, arg2) { corpo }
+
+// function cumprimentar(nome) {
+//     console.log("Olá " + nome);
+// }
+// cumprimentar("Carlos");
+
+// //escopo de variaveis
+
+// let cor = "azul";
+// //let cor = "preto";
+// /* obs: duas variaveis de mesmo nome não podem existir 
+// no mesmo local */
+
+// function mostraCor() {
+//     let cor = "verde";
+//     console.log(cor);
+// }
+
+// console.log(cor);
+// mostraCor();
+
+// //hoisting = içamento
+
+// testeHoisting(); // isso aqui em outra linguagens não existe
+
+// function testeHoisting()
+// {
+//     console.log("Deu tudo certo!");
+// }
+
+// arrow function
+
+// const testeArrow = () => console.log("Arrow function");
+// testeArrow()
+
+// truty and falsy
+
+// const minhaVariavel = ""; // falsy
+
+// if (minhaVariavel) {
+//     console.log("é verdadeiro");
+// } else {
+//     console.log("é falso");
+// }
+// const minhaVariave2 = "um texto"; // truthy
+
+// if (minhaVariave2) {
+//     console.log("é verdadeiro 2");
+// } else {
+//     console.log("é falso 2");
+// }
+
+
+// const numeros = [1, 2, 3, 4, 5];
+
+// console.log(numeros)
+// console.log(numeros[0]) // o indice sempre começa em 0 
+// console.log(numeros[2]) 
+// numeros.push(6) // novo numero adicionado
+// console.log(numeros)
+// numeros.pop() // excluir o ultimo numero
+// console.log(numeros)
+
+// string 
+//concatenação = +
+// const string1 = "Um texto";
+// const string2 = string1 + " muito legal";
+// console.log(string2)
+
+// //interpolação
+
+// const string3 = `${string2} como você esta?`
+// console.log(string3)
+// console.log(string3.toUpperCase())
+// console.log(string3.toLowerCase())
+
+//data e hora 
+
+// const agora = Date()
+// console.log(agora)
+
+// const natal = new Date(2023, 11, 25)
+// console.log(natal)
+
+//Math
+console.log(Math.PI);
+console.log(Math.round(3.6)); //arredonda um numero
+console.log(Math.sqrt(16)) // raiz quadrada
+console.log(Math.pow(2, 3)) //potencia o primeiro é a base e o segundo o expoente
+
---
+++ fundamentos/fundamentos_exercicios.js
@@ -0,0 +1,30 @@
+// Exercicios de fundamentos 
+
+// Exercício 1: Olá, Mundo!
+//Criar um script que imprima "Olá, Mundo!" no console
+// console.log("Olá Mundo");
+
+
+
+
+
+// Exercício 2: Conversão de tipo
+// Dado o valor de uma string "1234", converta-o em um número e exiba o tipo no console
+// const valor = "1234";
+// MeuNovoValor = Number(valor);
+// console.log(typeof MeuNovoValor);
+
+
+
+
+
+// Exercicio 3: Manipulação de strings 
+// Dado uma string "Javascript é incrível", escreva o código que conte quantos caracteres tem e quantas palavras existem na frase
+const ValorString = "Javascript é incrível";
+const NumeroDeCaracteres = ValorString.length;
+const NumeroDePalavras = ValorString.split(" ").length;
+console.log(`O numero de caracteres é ${NumeroDeCaracteres} 
+e o numero de palavras é ${NumeroDePalavras}`)
+/* Split é um metodo que divide uma string em palavras e 
+entregar uma lista de palavras que tem na string */
+
