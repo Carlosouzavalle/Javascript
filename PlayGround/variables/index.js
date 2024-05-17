e91211b886be04c6029cd2e75291146d998ec58c
Carlosouzavalle <carlosouzavalle@gmail.com>
2024-05-16 23:06:34 -0300

    something new

---
+++ PlayGround/variables/index.js
@@ -0,0 +1,7 @@
+let Namee = 'Carlos'
+let SecondName = 'Eduardo'
+let idade = 25
+
+document.getElementById('p1').textContent = `Your name is ${Namee}`
+document.getElementById('p2').textContent = SecondName
+document.getElementById('p3').textContent = idade
