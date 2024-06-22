e8feef8fe54081515d0b102a5bf19e1fb59a1154
carlosouzavalle <carlosouzavalle@gmail.com>
2024-06-21 23:18:56 -0300

    um pouco mais sobre callbacks

--- JS assincrono/index.js
+++ JS assincrono/index.js
@@ -1,14 +1,35 @@
 /* Uma função callback ela é passada para ser executada dps de alguns segundos */
-const loginUser = (email, password, callback) => {
+const loginUser = (email, password, onSucess, onError) => {
     setTimeout(() => {  
+        const error = false
+
+
+        if(error) {
+            return onError(new Error('error in loggin!'))
+        }
         console.log('User Logged!')
-        callback([ email ])
+        onSucess([ email ])
     }, 1500);
+
+}
+
+
+const getUserVideos = (email, callback) => {
+    setTimeout(() => {
+        callback(['video1', 'video2', 'video3'])
+    }, 2000);
 }
 
 const user = loginUser('teste123@gmail.com', '123456', (user) => {
-    console.log({user})
+    getUserVideos(user.email, (videos) => {
+        console.log({videos})
+    })
+}, (error) => {
+    console.log({error})
 })
+
+
+
 console.log([user])
 
 
